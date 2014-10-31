483fbe92af0bf5428525e08d24c0cc1ff5f72c66
Frederico Silva <fredericojssilva@gmail.com>
2014-10-31 17:53:18 +0000

    First commit

---
+++ src/net/frederico/showtipsview/ShowTipsViewInterface.java
@@ -0,0 +1,8 @@
+package net.frederico.showtipsview;
+/**
+ * @author Frederico Silva (fredericojssilva@gmail.com)
+ * @date Oct 31, 2014
+ */
+public interface ShowTipsViewInterface {
+	public void gotItClicked();
+}
